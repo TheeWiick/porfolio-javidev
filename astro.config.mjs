0229295927c56d7104486be1d461fcfa9fcd3976
TheeWiick <javiimartinez05@gmail.com>
2025-11-12 19:17:47 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    // ¡CAMBIA ESTO por el nombre exacto de tu repositorio!
+    base: '/porfolio-javidev/',
+    // ...otras configuraciones...
+});
